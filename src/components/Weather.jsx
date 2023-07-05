1e3107d8a1c9e8a09e9a44ab9d1185fb209fd34b
Mahmoud <mahmoud-bensaleh@outlook.com>
2023-07-05 13:53:27 +0300

    fix

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -6,28 +6,27 @@ const API_KEY = "14c0cd4ace1af244500f3292e2359919";
 
 const Weather = () => {
   const [weatherData, setWeatherData] = useState(null);
-  const [fiveDaysWeather, setFiveDaysWeather] = useState([]);
   const success = (position) => {
     const latitude = position.coords.latitude;
     const longitude = position.coords.longitude;
     console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
   };
 
-  const filter5Days = (data) => {
-    const fiveDaysArray = [];
-    for (let i = 0; i < data.length; i++) {
-      if (i - 1 !== -1) {
-        const convertDate1 = new Date(data[i - 1].dt * 1000).toLocaleString();
-        const convertDate2 = new Date(data[i].dt * 1000).toLocaleString();
-        const formatedDate1 = convertDate1.split(",")[0];
-        const formatedDate2 = convertDate2.split(",")[0];
-        if (formatedDate1 !== formatedDate2) fiveDaysArray.push(data[i]);
-      }
-    }
+  // const filter5Days = (data) => {
+  //   const fiveDaysArray = [];
+  //   for (let i = 0; i < data.length; i++) {
+  //     if (i - 1 !== -1) {
+  //       const convertDate1 = new Date(data[i - 1].dt * 1000).toLocaleString();
+  //       const convertDate2 = new Date(data[i].dt * 1000).toLocaleString();
+  //       const formatedDate1 = convertDate1.split(",")[0];
+  //       const formatedDate2 = convertDate2.split(",")[0];
+  //       if (formatedDate1 !== formatedDate2) fiveDaysArray.push(data[i]);
+  //     }
+  //   }
 
-    setFiveDaysWeather(fiveDaysArray);
-    console.log("fdw", fiveDaysWeather);
-  };
+  //   setFiveDaysWeather(fiveDaysArray);
+  //   console.log("fdw", fiveDaysWeather);
+  // };
 
   const error = () => {
     console.log("unable to retrive location");
@@ -53,12 +52,13 @@ const Weather = () => {
         console.log(response.data);
         // Update the weather data state with the retrieved data
         await setWeatherData(response.data);
-        filter5Days(weatherData.list);
       } catch (error) {
         console.log(error);
       }
     });
   }, []);
+
+  
 
   return (
     <div>
@@ -74,13 +74,13 @@ const Weather = () => {
         }}
       >
         {weatherData ? (
-          fiveDaysWeather.map((item) => (
+          weatherData.list.slice(0,5).map((item) => (
             <WeatherCard
               key={item.dt}
               city={weatherData.city.name}
               date={new Date(item.dt * 1000).toLocaleDateString()}
               temperature={Math.floor(item.main.temp / 10)}
-              feelsLike={Math.floor(item.main.feels_like)}
+              feelsLike={Math.floor}
               description={item.weather[0].description}
             />
           ))
